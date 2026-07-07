6a0803f00491446df30ce573dd3bb227e6c59dea
Brendan Ryan <1572504+brendanjryan@users.noreply.github.com>
2026-07-07 19:54:09 -0400

    fix: simplify mpp rewardkit criteria constants (#33)

--- shared/rewardkit-package/tempo_bench_rewardkit/common/__init__.py
+++ shared/rewardkit-package/tempo_bench_rewardkit/common/__init__.py
@@ -2,16 +2,12 @@ from .checks import TokenEfficiencyConfig, register_token_efficiency_check
 from .constants import (
     WORKSPACE_PATH,
     HttpStatus,
-    OutKey,
-    ScorePath,
     SourcePattern,
     WorkspaceFile,
 )
 
 __all__ = [
     "HttpStatus",
-    "OutKey",
-    "ScorePath",
     "SourcePattern",
     "TokenEfficiencyConfig",
     "WORKSPACE_PATH",
